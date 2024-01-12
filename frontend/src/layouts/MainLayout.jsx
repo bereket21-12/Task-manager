709774908f4f247e03a9b3afb642961e754b5cb1
Abel <abelzeleke5173@gmail.com>
2024-01-12 10:36:46 +0300

    made the some minor changes in the forms feild

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -4,12 +4,12 @@ import Navbar from '../components/Navbar';
 const MainLayout = ({ children }) => {
   return (
     <>
-      <div className='relative bg-gray-50 h-screen w-screen overflow-x-hidden'>
+      <div className="bg-gradient-to-t from-emerald-700 from-30%  to-gray-900 to-90% relative h-screen w-screen overflow-x-hidden">
         <Navbar />
         {children}
       </div>
     </>
-  )
+  );
 }
 
 export default MainLayout;
